714fd86e7cc3a7c97206670c40a469a1b19a3bef
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-03-15 22:23:53 +0200

    Update cashe.py

--- Examples/cashe.py
+++ Examples/cashe.py
@@ -2,17 +2,17 @@ import asyncio
 import starrailcard
 
 async def main():
-    async with starrailcard.MiHoMoCard(cashe = {"maxsize": 500, "ttl": 60}) as card:
+    async with starrailcard.Card(cashe = {"maxsize": 500, "ttl": 60}) as card:
         print(card)
 
     #OR
 
-    async with starrailcard.MiHoMoCard(cashe = {"maxsize": 500}) as card:
+    async with starrailcard.Card(cashe = {"maxsize": 500}) as card:
         print(card)
 
     #OR
 
-    async with starrailcard.MiHoMoCard(cashe = {"ttl": 60}) as card:
+    async with starrailcard.Card(cashe = {"ttl": 60}) as card:
         print(card)
         
 asyncio.run(main())
