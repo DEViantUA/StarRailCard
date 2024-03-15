4ee4d0afbc5249d98f1ac1b8fb7026e89d8cffd1
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-03-15 22:24:06 +0200

    Update character_id.py

--- Examples/character_id.py
+++ Examples/character_id.py
@@ -2,7 +2,7 @@ import asyncio
 import starrailcard
 
 async def main():
-    async with starrailcard.MiHoMoCard(character_id = "1302,1305, 1006") as card:
+    async with starrailcard.Card(character_id = "1302,1305, 1006") as card:
         print(card)
         
 asyncio.run(main())
