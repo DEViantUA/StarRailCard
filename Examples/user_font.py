fdde576470f4e7c72c6193246d1200a8d598a5f5
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-03-15 22:24:13 +0200

    Update user_font.py

--- Examples/user_font.py
+++ Examples/user_font.py
@@ -2,10 +2,10 @@ import asyncio
 import starrailcard
 
 async def main():
-    async with starrailcard.MiHoMoCard(user_font = "genshin.ttf") as card:
+    async with starrailcard.Card(user_font = "genshin.ttf") as card:
         print(card)
     #OR
-    async with starrailcard.MiHoMoCard(user_font = "C:/Users/Username/Documents/Fonts/genshin.ttf") as card:
+    async with starrailcard.Card(user_font = "C:/Users/Username/Documents/Fonts/genshin.ttf") as card:
         print(card)
       
 asyncio.run(main())
