f111bdfb125187d7a5d1179714e6be560cc068a0
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-03-15 23:11:29 +0200

    Create set_lang.py

---
+++ Examples/set_lang.py
@@ -0,0 +1,9 @@
+import asyncio
+import starrailcard
+
+async def main():
+    async with starrailcard.Card(lang = "en") as card:
+        card.set_lang("ua")
+        #Then you can call the main functions creat() and creat_profile()
+        
+asyncio.run(main())
