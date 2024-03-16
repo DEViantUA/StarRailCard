9207a5b8e31ae0633686e3cb3615aeee28fddbdb
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-03-17 00:57:14 +0200

    Update save_gif.py

--- Examples/save_gif.py
+++ Examples/save_gif.py
@@ -1,10 +1,11 @@
 import asyncio
 import starrailcard
 
-async with starrailcard.Card() as card:
-        data = await card.creat(700649319, style=2)
-    for card in data.card:
-        if card.animation:
-            card.save_gif(method = "pillow", format= "webp")
+async def main():
+        async with starrailcard.Card() as card:
+                data = await card.creat(700649319, style=2)
+            for card in data.card:
+                if card.animation:
+                    card.save_gif(method = "pillow", format= "webp")
       
-asyncio.run(mains())
+asyncio.run(main())
