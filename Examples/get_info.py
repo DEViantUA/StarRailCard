ce43f0e93bd4579cf17da5dd3dd4fc1dd837173e
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-03-16 23:00:15 +0200

    Create get_info.py

---
+++ Examples/get_info.py
@@ -0,0 +1,9 @@
+import asyncio
+import starrailcard
+
+async with starrailcard.Card() as card:
+        data = await card.creat(700649319, style=2)
+    for card in data.card:
+      await card.get_info(lang = "en")
+     
+asyncio.run(mains())
