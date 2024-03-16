c77a7834b4989d3e8378a268a4c08bcab0ebcb16
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-03-17 00:56:30 +0200

    Update get_info.py

--- Examples/get_info.py
+++ Examples/get_info.py
@@ -1,9 +1,10 @@
 import asyncio
 import starrailcard
 
-async with starrailcard.Card() as card:
-        data = await card.creat(700649319, style=2)
-    for card in data.card:
-      await card.get_info(lang = "en")
-     
+async def mains()
+        async with starrailcard.Card() as card:
+                data = await card.creat(700649319, style=2)
+            for card in data.card:
+              await card.get_info(lang = "en")
+             
 asyncio.run(mains())
