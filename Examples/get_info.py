560cb40983872aec2f124498c7055e6403ba1f31
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-03-17 22:58:32 +0200

    Update get_info.py

--- Examples/get_info.py
+++ Examples/get_info.py
@@ -5,6 +5,7 @@ async def main():
         async with starrailcard.Card() as card:
                 data = await card.creat(700649319, style=2)
             for card in data.card:
-              await card.get_info(lang = "en")
+              info = await card.get_info(lang = "en")
+              print(info)
              
 asyncio.run(main())
