a6ec639bf219d96cde91968803bfd69acc8342d4
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-05-18 00:20:04 +0300

    Update color.py
    Signed-off-by: Deviant <112615715+DEViantUA@users.noreply.github.com>

--- Examples/color.py
+++ Examples/color.py
@@ -2,7 +2,7 @@ import asyncio
 import starrailcard
 
 async def main():
-    async with starrailcard.Card(color = {"1308": ((98,47,104))}) as card:
+    async with starrailcard.Card(color = {"1308": (98,47,104)}) as card:
         await card.create(700649319, style=1)
         
 asyncio.run(main())
